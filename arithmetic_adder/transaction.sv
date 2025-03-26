4e73260636bca13fa9cf80646b11b13c7c9b64d8
Nithish Roy <roynithish70@gmail.com>
2025-03-26 20:34:51 +0530

    Create transaction.sv

---
+++ arithmetic_adder/transaction.sv
@@ -0,0 +1,20 @@
+class transaction;
+  
+  rand byte a;
+  
+  rand byte b;
+  
+ 	   byte c;
+  
+//    event my_event;
+  
+  function void display( string str);
+    
+    $display(str);
+    
+    //$display($time,"a = %0d b = %0d", a, b, c);
+  	
+  endfunction
+  
+endclass
+
